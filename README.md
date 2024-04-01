668303e3d8eb2609d81c3a58802300fa300a0a56
Faber <103538724+FaberTheCatgirl@users.noreply.github.com>
2024-03-31 21:06:59 -0700

    Asahi

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# TagTool
-[![Build status](https://ci.appveyor.com/api/projects/status/1ywqybpcsrh5ec5b/branch/dev?svg=true)](https://ci.appveyor.com/project/Beatthezombie/tagtool-obbr2/branch/dev)
+# Universal Tag Editor
+**A command-line tool forked from TagTool designed to aid in the research and modification of Halo cache files.** 
+---------
+![Screenshot](http://raw.github.com/FaberTheCatgirl/HaloShaderGenerator/master/doc/images/UTE%20v3.1-Alpha.png)
+# Halo Shader Generator
+*Halo Shader Generator is a library used by Universal Tag Editor and TagTool for generating templated shaders compatible with Halo Online*
+---------
+![Screenshot](http://raw.github.com/FaberTheCatgirl/HaloShaderGenerator/master/doc/images/concept.jpg)
 
-A command-line tool designed to aid in the research and modification of Halo cache files. Currently supports Gen3 (halo3, odst, partial support for reach) and Halo Online (ms23, partial support for higher versions).
+# Quick Links
+---------
+* **Universal Tag Editor**: A Fork of TagTool focused on Extended Version Support - https://github.com/FaberTheCatgirl/Universal-Tag-Editor/tags/
+* **HaloShaderGenerator**: A Fork of Halo Shader Generator Focused on Supporting Universal Tag Editor - https://github.com/FaberTheCatgirl/HaloShaderGenerator
+---------
+* *TagTool*: TagTool for manipulating Halo games - https://github.com/TheGuardians-CI/TagTool/releases/
+* *HaloShaderGenerator*: Halo Shader Generator is a library used by TagTool for generating templated shaders compatible with Halo Online MS23 - https://github.com/Beatthezombie/HaloShaderGenerator
--- TagTool/Cache/CacheVersionDetection.cs
+++ TagTool/Cache/CacheVersionDetection.cs
@@ -53,12 +53,16 @@ namespace TagTool.Cache
         {
             switch (buildName)
             {
-                case "01.09.25.2247":
+                case "01.01.14.2342": // Halo 1 Xbox PAL
+                case "01.03.14.0009": // Halo 1 Xbox Japan
+                case "01.12.09.0135": // Halo 1 Xbox China
                 case "01.10.12.2276":
                     version = CacheVersion.HaloXbox;
                     cachePlatform = CachePlatform.Original;
                     break;
+                case "01.00.00.0576": // Halo 1 PC Trial
                 case "01.00.00.0564":
+                case "01.00.10.0621": // Halo 1 PC v10
                     version = CacheVersion.HaloPC;
                     cachePlatform = CachePlatform.Original;
                     break;
@@ -74,16 +78,24 @@ namespace TagTool.Cache
                     version = CacheVersion.Halo2Xbox;
                     cachePlatform = CachePlatform.Original;
                     break;
+                case "11028.07.03.23.1927.main": // Halo Vista Review Build
                 case "11081.07.04.30.0934.main":
+                case "11091.07.05.11.1104.main":
                 case "11122.07.08.24.1808.main":
                     version = CacheVersion.Halo2Vista;
                     cachePlatform = CachePlatform.Original;
                     break;
+                case "08117.07.03.07.1702.delta": // This is the March 7th Build of the Halo 3 Beta, I have added support for this build to allow the addition of cut content such as a player and enemy models that are not present in retail.
+                case "08172.07.03.08.2240.delta": // This is the March 8th Build of the Halo 3 Beta, not much is diffrent much March 7th. but for version detection purposes this is needed as some Tag offsets are diffrent.
+                case "Mar  9 2007 22:22:32": // This version is unique in which is uses the "343i" Style of Timestamping builds as opposed to the Bungie Style of Timestamping builds. This is the March 9th Build of the Halo 3 Beta, this build is the first to have the new build naming style.
+                case "Mar 10 2007 16:16:44": // This is the March 10th Build of the Halo 3 Beta, this build is the second to have the new build naming style.
                 case "09699.07.05.01.1534.delta":
                    version = CacheVersion.Halo3Beta;
                     cachePlatform = CachePlatform.Original;
                     break;
+                case "11729.07.08.10.0021.main": // This is the Halo 3 Expo Build shown at E3 2007.
                 case "11855.07.08.20.2317.halo3_ship":
+                case "11856.07.08.20.2332.release": // This is the Halo 3 Epsilon Build.
                 case "12065.08.08.26.0819.halo3_ship":
                     version = CacheVersion.Halo3Retail;
                     cachePlatform = CachePlatform.Original;
@@ -160,6 +172,16 @@ namespace TagTool.Cache
                     version = CacheVersion.HaloOnline700123;
                     cachePlatform = CachePlatform.Original;
                     break;
+                case "08516.10.02.19.1607.omaha_alpha":
+                    version = CacheVersion.HaloReachAlpha;
+                    cachePlatform = CachePlatform.Original;
+                    break;
+                case "09449.10.03.25.1545.omaha_beta": // This is the Halo Reach Pre-Beta Build. This was used to internally Test the game before the Beta was released.
+                case "09730.10.04.09.1309.omaha_delta": // This is the Halo Reach Beta Build. This was used to publically Test the game before the it was released.
+                    version = CacheVersion.HaloReachBeta; // This is the Added Version Detection for the Halo Reach Beta Build, I have had to manually map out Tag Offsets for this build as they are not present in the Halo Reach Retail Build. This is due to the fact that the Halo Reach Beta Build is a heavily modified version of the Halo 3 Engine and requires some special handling.
+                    cachePlatform = CachePlatform.Original;
+                    break;
+                case "00095.11.04.09.1509.demo": // This is the Halo Reach Demo, which ran on a seprate build of the Halo Reach Engine. This is the Added Version Detection for the Halo Reach Demo Build, I have had to manually map out Tag Offsets for this build as they are not present in the Halo Reach Retail Build. This is due to the fact that the Halo Reach Demo Build is a modified version of the Halo Reach Retail Build and requires some special handling.
                 case "11860.10.07.24.0147.omaha_relea":
                     version = CacheVersion.HaloReach;
                     cachePlatform = CachePlatform.Original;
@@ -176,16 +198,108 @@ namespace TagTool.Cache
                     cachePlatform = CachePlatform.Original;
                     break;
 
-                case "May 29 2019 00:44:52":
-                case "Jun 24 2019 00:36:03":
-                case "Jul 30 2019 14:17:16":
-                case "Oct 24 2019 15:56:32":
-                    version = CacheVersion.HaloReach;
+                case "May 29 2019 00:44:52": // MCC Internal Test Build
+                case "Jun 24 2019 00:36:03": // MCC Flight 1
+                case "Jul 30 2019 14:17:16": // MCC Flight 2
+                case "Oct 24 2019 15:56:32": // MCC Day One Build
+                case "Jan 30 2020 16:55:25": // MCC Update 1
+                case "Mar 24 2020 12:10:36": // MCC Update 2
+                case "Jun  5 2020 10:40:14": // MCC Update 3
+                case "Oct 15 2020 18:23:50": // MCC Update 4
+                case "Nov 24 2020 18:32:37": // MCC Update 5
+                case "Feb 17 2021 21:07:38": // MCC Flight 3
+                case "Mar  4 2021 13:14:28": // MCC Update 6
+                case "May 26 2021 10:02:45": // MCC Update 7
+                case "Aug 11 2021 15:50:30": // MCC Flight 4
+                case "Sep 13 2021 09:49:52": // MCC Update 8
+                case "Sep 17 2021 13:25:40": // MCC Update 9
+                case "Jan 13 2022 00:54:50": // MCC Update 10
+                case "Aug  5 2022 20:35:02": // MCC Update 11
+                case "Aug 31 2022 11:53:49": // MCC Mod Tools Release
+                case "Aug 31 2022 11:53:19": // MCC Mod Tools Release (Tool_Fast)
+                case "Oct 12 2022 01:55:01": // MCC Update 12
+                case "Nov 16 2022 21:11:25": // MCC Mod Tools Update 1
+                case "Nov 16 2022 21:13:04": // MCC Mod Tools Update 1 (Tool_Fast)
+                case "Jun 21 2023 15:35:31": // MCC Update 13
+                case "Jun 27 2023 08:55:51": // MCC Mod Tools Update 2
+                case "Jun 27 2023 08:55:19": // MCC Mod Tools Update 2 (Tool_Fast)
+                case "Jul 16 2023 16:12:13": // MCC Mod Tools Update 3
+                case "Jul 16 2023 16:08:14": // MCC Mod Tools Update 3 (Tool_Fast)
+                    version = CacheVersion.HaloReach; // I have added a complete MCC Version History for the Cache Detection System, this is the MCC Version Detection for Halo Reach.
                     cachePlatform = CachePlatform.MCC;
                     break;
-
-                case "Sep 29 2021 09:17:56":
-                    version = CacheVersion.Halo3Retail;
+                case "Jun  4 2020 20:29:31": // MCC Flight 1
+                case "Jun 21 2020 16:34:20": // MCC Flight 2
+                case "Jun 25 2020 15:02:49": // MCC Day 1
+                case "Aug 11 2020 23:34:41": // MCC Flight 3
+                case "Aug 26 2020 21:24:11": // MCC Update 1
+                case "Oct  7 2020 03:55:07": // MCC Flight 4 Main Menu Map
+                case "Oct 21 2020 09:24:30": // MCC Update 2
+                case "Nov 24 2020 15:47:48": // MCC Update 3
+                case "Feb 19 2021 11:19:43": // MCC Flight 4
+                case "Mar  5 2021 08:45:13": // MCC Flight 5
+                case "Mar 14 2021 03:19:55": // MCC Update 4
+                case "May 19 2021 16:19:54": // MCC Flight 6
+                case "Jun  9 2021 09:25:41": // MCC Update 5
+                case "Aug 25 2021 03:43:53": // MCC Flight 7
+                case "Sep 29 2021 09:17:56": // MCC Update 6
+                case "Oct 11 2021 04:58:42": // MCC Update 7
+                case "Oct 11 2021 04:56:39": // MCC Mod Tools Release
+                case "Nov  1 2021 06:16:14": // MCC Update 8
+                case "Nov  1 2021 06:14:30": // MCC Mod Tools Update 1
+                case "Feb 15 2022 01:35:34": // MCC Update 9
+                case "Apr  6 2022 10:48:27": // MCC Mod Tools Update 2
+                case "Apr  6 2022 10:48:46": // MCC Mod Tools Update 2 (Tool_Fast)
+                case "Apr 20 2022 11:55:21": // MCC Mod Tools Update 3
+                case "Apr 20 2022 11:56:23": // MCC Mod Tools Update 3 (Tool_Fast)
+                case "May  3 2022 12:38:41": // MCC Update 10
+                case "Jun  8 2022 16:05:04": // MCC Mod Tools Update 4
+                case "Jun  8 2022 16:06:30": // MCC Mod Tools Update 4 (Tool_Fast)
+                case "Jul 21 2022 21:15:27": // MCC Update 11
+                case "Aug 21 2022 17:23:29": // MCC Mod Tools Update 5
+                case "Aug 21 2022 17:24:39": // MCC Mod Tools Update 5 (Tool_Fast)
+                case "Oct 11 2022 04:34:53": // MCC Update 12
+                case "Nov 16 2022 21:16:57": // MCC Mod Tools Update 6
+                case "Nov 16 2022 21:18:10": // MCC Mod Tools Update 6 (Tool_Fast)
+                case "May 22 2023 20:10:02": // MCC Update 13 
+                case "Jun 27 2023 08:39:36": // MCC Mod Tools Update 7 (Tool and Tool_Fast are the same in this update)
+                case "Sep  1 2023 11:01:27": // MCC Mod Tools Update 8
+                case "Sep  1 2023 11:01:50": // MCC Mod Tools Update 8 (Tool_Fast)
+                case "Dec 21 2023 22:31:37": // MCC Update 14
+                case "Jan  3 2024 10:08:58": // MCC Mod Tools Update 9
+                case "Jan  3 2024 10:01:45": // MCC Mod Tools Update 9 (Tool_Fast)
+                    version = CacheVersion.Halo3Retail; // I have added a complete MCC Version History for the Cache Detection System, this is the MCC Version Detection for Halo 3.
+                    cachePlatform = CachePlatform.MCC;
+                    break;
+                case "Aug 11 2020 06:58:27": // MCC Insider Build 1
+                case "Aug 17 2020 01:12:27": // MCC Insider Build 2
+                case "Aug 24 2020 08:37:26": // MCC Insider Build 3
+                case "Aug 28 2020 08:43:36": // MCC Day One Build
+                case "Sep 29 2020 10:59:04": // MCC Update 1
+                case "Dec  4 2020 18:24:06": // MCC Update 2
+                case "Aug 25 2021 03:35:05": // MCC Insider Build 4
+                case "Sep 18 2021 21:15:55": // MCC Update 3
+                case "Feb 15 2022 01:59:47": // MCC Update 4
+                case "Apr  5 2022 14:16:52": // MCC Mod Tools Day One
+                case "Apr  5 2022 14:16:35": // MCC Mod Tools Day One (Tool Fast Enabled Build)
+                case "Apr 20 2022 11:53:15": // MCC Mod Tools Update 1 / MCC Update 5
+                case "Apr 20 2022 11:52:14": // MCC Mod Tools Update 1 (Tool Fast Enabled Build)
+                case "May  3 2022 12:27:56": // MCC Mod Tools Update 2
+                case "May  3 2022 12:27:08": // MCC Mod Tools Update 2 (Tool Fast Enabled Build)
+                case "Jul 22 2022 05:55:30": // MCC Update 6
+                case "Aug 21 2022 17:29:59": // MCC Mod Tools Update 3
+                case "Aug 21 2022 17:30:28": // MCC Mod Tools Update 3 (Tool Fast Enabled Build)
+                case "Oct 24 2022 03:07:19": // MCC Update 7
+                case "Nov 16 2022 20:57:46": // MCC Mod Tools Update 4
+                case "Nov 16 2022 20:59:16": // MCC Mod Tools Update 4 (Tool Fast Enabled Build)
+                case "May 16 2023 11:44:41": // MCC Update 8
+                case "Jun 27 2023 08:39:51": // MCC Mod Tools Update 5
+                case "Jun 27 2023 08:39:50": // MCC Mod Tools Update 5 (Tool Fast Enabled Build)
+                case "Jul 16 2023 15:51:04": // MCC Mod Tools Update 6
+                case "Jul 16 2023 15:52:04": // MCC Mod Tools Update 6 (Tool Fast Enabled Build)
+                case "Sep  1 2023 10:55:15": // MCC Mod Tools Update 7
+                case "Sep  1 2023 10:55:29": // MCC Mod Tools Update 7 (Tool Fast Enabled Build)
+                    version = CacheVersion.Halo3ODST; // I have added a complete MCC Version History for the Cache Detection System, this is the MCC Version Detection for Halo 3 ODST. Which isn't present in the current dev branch of TagTool.
                     cachePlatform = CachePlatform.MCC;
                     break;
                 default:
@@ -208,7 +322,9 @@ namespace TagTool.Cache
                 switch (version)
                 {
                     case CacheVersion.Halo3Retail:
-                        return "Sep 29 2021 09:17:56";
+                        return "Jan  3 2024 10:08:58";
+                    case CacheVersion.Halo3ODST:
+                        retuen "Sep  1 2023 10:55:15";
                     default:
                         return version.ToString();
                 }
